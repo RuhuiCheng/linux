d3a39105f6fa7f7fd889950f756530f319d0dd80
RuhuiCheng <chengruhui@outlook.com>
2019-09-10 14:19:19 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -2,3 +2,8 @@
 export JAVA_HOME=/usr/share/jdk1.6.0_14 
 export PATH=$JAVA_HOME/bin:$PATH 
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar 
+### spark
+export SPARK_HOME = /home/hadoop/spark-2.1.0-bin-hadoop2.7
+export PATH = $PATH:/home/hadoop/spark-2.1.0-bin-hadoop2.7/bin
+export PYTHONPATH = $SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
+export PATH = $SPARK_HOME/python:$PATH
