9cce28de9875dcdc1b78a4f2541784eef2d72c9e
jason.cheng <chengruhui@outlook.com>
2020-03-17 22:21:04 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -3,6 +3,16 @@ export JAVA_HOME=/mnt/c/linux/opt/jdk1.8.0_201
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 
+# Hadoop Environment Variables
+export HADOOP_HOME=/opt/hadoop-2.7.7
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+
 ### maven
 export PATH=/mnt/c/linux/opt/apache-maven-3.6.0/bin:$PATH
 
